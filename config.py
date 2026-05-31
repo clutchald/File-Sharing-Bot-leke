367bbce556fd5b79c254ec355969ef55b6d95eeb
clutchald <legitclutch94@gmail.com>
2026-05-31 20:41:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ DB_URL = os.environ.get("DB_URL", "mongodb+srv://sanjusen212121:dRTWTRgVX19SJwTk
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002601717681"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1003875208767"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003875208767"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002601717681"))
 
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
